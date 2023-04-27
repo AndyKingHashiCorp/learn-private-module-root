769f6449e0a4284282f7ca885b413a7d6f9cc53c
Andy King @ HashiCorp <126875389+AndyKingHashiCorp@users.noreply.github.com>
2023-04-27 12:05:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0.0"
-    }
-  }
+provider "aws" {
+  region = var.region
 }
-
 
 module "s3-webapp" {
   source  = "app.terraform.io/Instruqt-Andy-King-Learning/s3-webapp/aws"
-  name   = var.name
-  region = var.region
-  prefix = var.prefix
+  name    = var.name
+  region  = var.region
+  prefix  = var.prefix
   version = "1.0.0"
 }
